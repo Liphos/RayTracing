e4e73488716983d936045bdbc08c34ec59270cf3
Liphos <levyguillaume10@yahoo.fr>
2024-09-08 11:58:07 +0200

    Format and add print normal sphere

--- first_image.cpp
+++ first_image.cpp
@@ -1,7 +1,8 @@
 #include "color.h"
 #include "vec3.h"
 #include <iostream>
-int main() {
+int main()
+{
     // Image
 
     int image_width = 256;
@@ -9,12 +10,15 @@ int main() {
 
     // Render
 
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << " " << image_height << "\n255\n";
 
-    for (int j=0; j< image_height; j++){        
+    for (int j = 0; j < image_height; j++)
+    {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i<image_width; i++){
-            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+        for (int i = 0; i < image_width; i++)
+        {
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
             write_color(std::cout, pixel_color);
         }
     }
